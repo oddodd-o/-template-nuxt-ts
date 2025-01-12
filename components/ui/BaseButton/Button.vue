b93cc8e6c3c9cc7d5a8ca5a8c8adb34ad24d29a0
odada-o <eehd80yul@gmail.com>
2025-01-12 11:19:52 +0900

    first commit

---
+++ components/ui/BaseButton/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="handleClick" class="custom-button">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import {ref, onMounted} from 'vue'
+
+const emit = defineEmits(['click'])
+
+const handleClick = () => {
+  emit('click')
+}
+</script>
+
+<style scoped>
+.custom-button {
+  padding: 8px 16px;
+  border-radius: 4px;
+  background-color: red;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+</style>
